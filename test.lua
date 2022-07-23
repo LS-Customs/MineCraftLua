e2f8bc2701f6316bbbb5e771b60fb594f3e57f6b
LS-Customs <leander.steckel@mailbox.org>
2022-07-23 13:08:16 +0200

    changed robot requirement

--- test.lua
+++ test.lua
@@ -1,8 +1,8 @@
-local robot = require("robot")
+-- local robot = require("robot")
 local component = require("component")
 
 function drill() do
     for i = 0, 3, 1 do 
-        robot.forward()
+        print(i)
     end
 end
